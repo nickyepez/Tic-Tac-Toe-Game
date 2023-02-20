bfc04706fcbed2bb0696ba96b36451464e66fad6
Nick Yepez <nickyepez@NICKs-Air.attlocal.net>
2023-02-20 13:25:16 -0600

    Pass ID As State From Parent To Child

--- colorGame00.js
+++ colorGame00.js
@@ -1,19 +1,23 @@
-const Square = () => {
-    return <button> </button>;
+const Square = ({ id }) => {
+    return (
+    <button>
+        <h1>{id}</h1>
+    </button>
+    );
 };
 
 const Board = () => {
     const [player, setPlayer] = React.useState(1);
     let status = `Player ${player}`;
     function rednerSquare(i) {
-        return <Square></Square>;
+        return <Square id={i}></Square>;
     }
     return (
         <div className="game-board">
             <div className="grid-row">
-            {rednerSquare(0)}
-            {rednerSquare(1)}
-            {rednerSquare(2)}
+                {rednerSquare(0)}
+                {rednerSquare(1)}
+                {rednerSquare(2)}
             </div>
             <div id="info">
                 <h1> {status} </h1>
