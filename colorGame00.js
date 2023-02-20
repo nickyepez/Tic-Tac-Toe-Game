d8c0e21ed922b25711231e1b0433a1b44068e34f
Nick Yepez <nickyepez@NICKs-Air.attlocal.net>
2023-02-20 13:14:10 -0600

    Add squares as child components

--- colorGame00.js
+++ colorGame00.js
@@ -1,18 +1,22 @@
+const Square = () => {
+    return <button> </button>;
+};
+
 const Board = () => {
     const [player, setPlayer] = React.useState(1);
     let status = `Player ${player}`;
+    function rednerSquare(i) {
+        return <Square></Square>;
+    }
     return (
-        <div
-            className="game-board"
-            onClick={e => {
-                setPlayer((player + 1) % 2); //the player needs to be 0 or 1
-                status = `Player ${player}`;
-                e.target.style.background = "red";
-                e.target.style.width = 600;
-            }}
-        >
+        <div className="game-board">
+            <div className="grid-row">
+            {rednerSquare(0)}
+            {rednerSquare(1)}
+            {rednerSquare(2)}
+            </div>
             <div id="info">
-                <h1>{status}</h1>
+                <h1> {status} </h1>
             </div>
         </div>
     );
